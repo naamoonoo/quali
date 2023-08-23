42d131dbb30bdfcd86a2cb58d27fb6ca54b946ee
naamoonoo <naamoonoo.y@gmail.com>
2023-08-24 00:16:22 +0900

    feat: main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,44 @@
 "use client";
 
 import { useItem } from "@/hooks/useItem";
+import { useState } from "react";
 import Link from "next/link";
+import { Logo } from "@/components/Logo";
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { ArrowRight } from "@/components/icons/ArrowRight";
 
 export default function Home() {
-  const { items, addItem } = useItem();
+  const [value, setValue] = useState("");
 
   return (
-    <div>
-      <h1>quali</h1>
-      <div>
-        {items.map((item) => (
-          <Link href={`/item/${item.id}`} key={item.id}>
-            {item.id}-{item.name}
-          </Link>
-        ))}
-      </div>
-      <button onClick={addNewItem}>add item</button>
+    <div className="flex flex-col justify-center items-center w-screen h-screen bg-main-background">
+      <Logo isDark size={100} className="mb-10" />
+      <p
+        style={{
+          color: "var(--text-primary, #212121)",
+          textAlign: "center",
+          /* header/large */
+          fontFamily: "Lato",
+          fontSize: "24px",
+          fontStyle: "normal",
+          fontWeight: "700",
+          lineHeight: "36px;",
+        }}
+      >
+        {`Hey, it's Quail. How can I assist you with your project today?`}
+      </p>
+      <Input
+        className="w-3/5 h-32 mt-4 mb-6"
+        value={value}
+        onChange={setValue}
+        placeholder="E.g., I can help create a modern homepage design or a draft for a mobile app interface..."
+      />
+      <Button
+        label="Let's go"
+        onClick={() => alert("hello")}
+        icon={<ArrowRight />}
+      />
     </div>
   );
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      "main-background": "#F5F5F5",
+      "button-focus-outline": "rgba(66, 0, 255, 1)",
+      "button-background": "#232125",
+      "button-shadow": "0px 4px 8px 0px rgba(0, 0, 0, 0.15)",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
